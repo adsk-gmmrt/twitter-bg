5995f5f739b0f96d675b850f072ff90683098ffe
Tomasz Domanski <Tomasz.Domanski@autodesk.com>
2016-07-20 11:30:08 +0200

    Separate views for maps and stats created

---
+++ server/content/map-view/map-view.component.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.
+  module('twigbro.mapView').
+  component('mapView', {
+    templateUrl: 'map-view/map-view.template.html',
+    controller: [ 
+      
+      function MapViewController() {
+
+      }
+    ]
+  });
---
+++ server/content/stat-view/stat-view.component.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.
+  module('twigbro.statView').
+  component('statView', {
+    templateUrl: 'stat-view/stat-view.template.html',
+    controller: [ 
+      
+      function StatViewController() {
+
+      }
+    ]
+  });
