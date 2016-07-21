ebf88999a58b5456da934b4243e844706ea27e04
Tomasz Domanski <Tomasz.Domanski@autodesk.com>
2016-07-21 10:08:30 +0200

    Stat view service update

--- server/content/stat-view/stat-view.service.js
+++ server/content/stat-view/stat-view.service.js
@@ -3,74 +3,89 @@
 angular.module('twigbro.statView')
   .factory('StatService', function () {
 
-    var getData = function () {
-      var cities = [
-        "Montgomery",
-        "Juneau",
-        "Phoenix",
-        "Little Rock",
-        "Sacramento",
-        "Denver",
-        "Hartford",
-        "Dover",
-        "Tallahassee",
-        "Atlanta",
-        "Honolulu",
-        "Boise",
-        "Springfield",
-        "Indianapolis",
-        "Des Moines",
-        "Topeka",
-        "Frankfort",
-        "Baton Rouge",
-        "Augusta",
-        "Annapolis",
-        "Boston",
-        "Lansing",
-        "St. Paul",
-        "Jackson",
-        "Jefferson City",
-        "Helena",
-        "Lincoln",
-        "Carson City",
-        "Concord",
-        "Trenton",
-        "Santa Fe",
-        "Albany",
-        "Raleigh",
-        "Bismarck",
-        "Columbus",
-        "Oklahoma City",
-        "Salem",
-        "Harrisburg",
-        "Providence",
-        "Columbia",
-        "Pierre",
-        "Nashville",
-        "Austin",
-        "Salt Lake City",
-        "Montpelier",
-        "Richmond",
-        "Olympia",
-        "Charleston",
-        "Madison",
-        "Cheyenne"
-      ]
+    var cities = [
+      "Montgomery",
+      "Juneau",
+      "Phoenix",
+      "Little Rock",
+      "Sacramento",
+      "Denver",
+      "Hartford",
+      "Dover",
+      "Tallahassee",
+      "Atlanta",
+      "Honolulu",
+      "Boise",
+      "Springfield",
+      "Indianapolis",
+      "Des Moines",
+      "Topeka",
+      "Frankfort",
+      "Baton Rouge",
+      "Augusta",
+      "Annapolis",
+      "Boston",
+      "Lansing",
+      "St. Paul",
+      "Jackson",
+      "Jefferson City",
+      "Helena",
+      "Lincoln",
+      "Carson City",
+      "Concord",
+      "Trenton",
+      "Santa Fe",
+      "Albany",
+      "Raleigh",
+      "Bismarck",
+      "Columbus",
+      "Oklahoma City",
+      "Salem",
+      "Harrisburg",
+      "Providence",
+      "Columbia",
+      "Pierre",
+      "Nashville",
+      "Austin",
+      "Salt Lake City",
+      "Montpelier",
+      "Richmond",
+      "Olympia",
+      "Charleston",
+      "Madison",
+      "Cheyenne"
+    ]
+
+    var data = [];
+
+    for (var i = Math.floor(Math.random() * 10 + 1); i < cities.length; i += 5) {
+      var totalVotes = Math.floor((Math.random() * 1000000) + 1);
+      var clinton = Math.random();
+      data.push([cities[i], clinton * totalVotes, (1 - clinton) * totalVotes]);
+    }
+
+    var getVotes = function (name) {
 
       var output = [
-        ['City', 'Votes for Clinton', 'Total votes']
-      ];
+        [
+          'City',
+          name === "CLINTON" ? 'Votes for Clinton' : 'Votes for Trump',
+          'Total votes'
+        ]
+      ]
 
-      for (var i = 0; i < cities.length; i++) {
-        var clinton = Math.random();
-        output.push([cities[i], clinton, Math.floor((Math.random() * 1000000) + 1)]);
+      for (var i = 0; i < data.length; i++) {
+        var total = data[i][1] + data[i][2];
+        if (total > 0) {
+          output.push([data[i][0], data[i][name === "CLINTON" ? 1 : 2] / total, total])
+        }
       }
 
       return output;
     }
 
     return {
-      getData: getData
+      getVotes: getVotes,
     }
 
   });
