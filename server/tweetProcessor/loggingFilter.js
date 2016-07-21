ea13193fbab3d50ba2db672d6b0afda27e780d29
MichalGwizdz <michal_gwizdz@o2.pl>
2016-07-21 12:01:21 +0200

    Log with coordinates and city

--- server/tweetProcessor/loggingFilter.js
+++ server/tweetProcessor/loggingFilter.js
@@ -6,7 +6,14 @@ var LoggingFilter = function() {
 LoggingFilter.KEY = 'loggingFilter';
 
 LoggingFilter.prototype.process = function(tweet) {
-  console.log("New tweet: " + tweet.text);
+  console.log("New tweet: ");
+  console.log("Text:",tweet.text);
+  console.log("Coordinates: ", tweet.coordinates.coordinates);
+  if(tweet.place && tweet.place_type == "city" && tweet.place.name){
+    console.log("City: ", tweet.place.name);
+  } else{
+    console.log("City is not defined");
+  }
 };
 
 LoggingFilter.prototype.getResult = function() {
