af978a46542748db8749f708193f368654bd5b05
Tomasz Domanski <Tomasz.Domanski@autodesk.com>
2016-07-22 09:03:52 +0200

    Coordinates completed

--- server/content/stat-view/stat-view.component.js
+++ server/content/stat-view/stat-view.component.js
@@ -4,53 +4,20 @@ angular.
   module('twigbro.statView').
   component('statView', {
     templateUrl: 'stat-view/stat-view.template.html',
-    controller: ['$routeParams', 'StatService',
+    controller: ['StatService',
 
-      function StatViewController($routeParams, StatService) {
+      function StatViewController(StatService) {
 
-        
-        // var API_KEY = "AIzaSyCYa7ZwoXJUYaApV9Xmz_mxWKfbtHEOjSM";
-        var createChart = function () {
+        this.loadData = function () {
+          var self = this;
+          StatService.getVotes().then(function (result) {
+            self.chart.data = result["data"];
+          });
+        }
 
-          var chart = {
-            type: "GeoChart",
-            cssStyle: "width: 800px; height: 400px;",
-            options: {
-              width: 1000,
-              height: 700,
-              chartArea: { left: 10, top: 10, bottom: 0, width: "100%" },
-              legend: { numberFormat: "0%" },
-              region: 'US',
-              displayMode: 'markers',
-              colorAxis: { colors: ['blue', 'red'] },
-              backgroundColor: '#81d4fa',
-            },
-            formatters: {
-              number: [{
-                columnNum: 1,
-                pattern: "0%"
-              }]
-            },
-            data: [
-              ['City', 'Clinton', 'Total'],
-              ['Boston', 0, 0]
-            ]
-          }
+        this.chart = StatService.createChart();
+        this.loadData();
 
-          return chart;
-        };
-
-        this.hasData = true;
-        this.chart = createChart();
-
-
-        var self = this;
-        StatService.getVotes($routeParams.name).then(function (data) {
-          var chart = createChart();
-          chart.data = data;
-          self.hasData = chart.data.length > 1;
-          self.chart = chart;
-        });
       }
     ]
   });
