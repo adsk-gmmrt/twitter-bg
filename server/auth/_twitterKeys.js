7ddb78e52a8f1b3164b07cca41acb6431a058d34
Maciej Sapun <Maciej.Sapun@autodesk.com>
2016-07-22 14:55:12 +0200

    update _twitterKeys.js

--- server/auth/_twitterKeys.js
+++ server/auth/_twitterKeys.js
@@ -4,5 +4,7 @@ module.exports = {
   consumer_key: 'YOUR_TWITTER_CONSUMER_KEY_HERE',
   consumer_secret: 'YOUR_TWITTER_CONSUMER_SECRET_HERE',
   token: 'YOUR_TWITTER_TOKEN_HERE',
-  token_secret: 'YOUR_TWITTER_TOKEN_SECRET_HERE'
+  access_token_key: 'YOUR_TWITTER_TOKEN_HERE', // repeated for easier use with request and twitter modules
+  token_secret: 'YOUR_TWITTER_TOKEN_SECRET_HERE',
+  access_token_secret: 'YOUR_TWITTER_TOKEN_SECRET_HERE' // repeated for easier use with request and twitter modules
 };
